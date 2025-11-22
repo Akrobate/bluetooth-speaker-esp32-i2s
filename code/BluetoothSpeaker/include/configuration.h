6d38fd826e2227872038f072dd05c50bb749d306
Akrobate <fedorov.artiom@gmail.com>
2025-11-22 17:45:29 +0100

    Added configurations for buttons, started testing on hardware

--- code/BluetoothSpeaker/include/configuration.h
+++ code/BluetoothSpeaker/include/configuration.h
@@ -4,8 +4,8 @@
 #define BLUETOOTH_NAME "MySpeaker"
 
 // Pins definition
-#define PIN_BUTTON_PLAY_PAUSE 1
+#define PIN_BUTTON_PLAY_PAUSE 25
 #define PIN_BUTTON_NEXT 2
 #define PIN_BUTTON_PREVIOUX 3
-#define PIN_VOLUME_UP 4
-#define PIN_VOLUME_DOWN 5
+#define PIN_VOLUME_UP 26
+#define PIN_VOLUME_DOWN 27
